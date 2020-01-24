a53517e734a4532e6ecc6736dec55306f32d6f2f
Holy JS <kebradooj@gmail.com>
2020-01-24 14:59:48 +0600

    #jeka: Basic settings fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
 import Header from './component/Header/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    modalIsShow: false
+  }
+
+  modalIsShow = event => {
+    this.setState({
+      modalIsShow: !this.state.modalIsShow,
+    })
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Header
+          
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 export default class Footer extends Component {
+  state = {
+    
+  }
+
+  closeModal = event => {
+    const className = event.target.className;
+
+    if(className !== 'content-menu' && className !== 'menu__closeBtn') {}
+  }
   render() {
     return (
       <div className='header'>
         <div className='container'>
           <div className='nav'>
-            <a href='/' className='nav-logo'></a>
+            <Link to='/' className='nav-logo'></Link>
             <div className='nav-content'>
-              <a className='content-add-bg' href='/modal'>
+              <Link className='content-add-bg' to='/modal'>
                 <div className='content-add'></div>
-              </a>
-              <a className='content-search' href='/search'></a>
-              <a className='content-menu' href='#menu' onClick={()=>{
-
-              }}></a>
+              </Link>
+              <Link className='content-search' to='/search'></Link>
+              <Link className='content-menu' onClick={console.log('CLICKED')}>
+                <button className="menu__closeBtn">X</button>
+                <ul>
+                  <li><Link to='/search'>Найти поездку</Link></li>
+                  <li><Link to='/offer'>Предложить поездку</Link></li>
+                  <li><Link to='/account/login'><b>Войти</b></Link></li>
+                  <li><Link to='/registration'><b>Регистрация</b></Link></li>
+                </ul>
+              </Link>
             </div>
           </div>
         </div>
